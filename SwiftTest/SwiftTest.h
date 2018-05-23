3bd530c5fbd25464810b6ab1c2ca476503a40ad8
Gowtham Kudupudi <gowtham@ferryfair.com>
2018-05-23 09:00:48 +0530

    Initial Commit

---
+++ SwiftTest/SwiftTest.h
@@ -0,0 +1,19 @@
+//
+//  SwiftTest.h
+//  SwiftTest
+//
+//  Created by Gowtham Kudupudi on 23/05/18.
+//  Copyright © 2018 ferryfair. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftTest.
+FOUNDATION_EXPORT double SwiftTestVersionNumber;
+
+//! Project version string for SwiftTest.
+FOUNDATION_EXPORT const unsigned char SwiftTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftTest/PublicHeader.h>
+
+
